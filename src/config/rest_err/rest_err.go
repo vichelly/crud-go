1e744d5a7e25d98809b90b2b39aff55d2dd31495
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2026-01-10 10:51:05 -0300

    Refactor error handling by adding NewRestErr and specific error constructors

--- src/config/rest_err/rest_err.go
+++ src/config/rest_err/rest_err.go
@@ -1,5 +1,7 @@
 package rest_err
 
+import "net/http"
+
 type RestErr struct {
 	Message string   `json:"message"`
 	Err     string   `json:"error"`
@@ -10,4 +12,38 @@ type RestErr struct {
 type Causes struct {
 	Field   string `json:"field"`
 	Message string `json:"message"`
+}
+
+func NewRestErr(message string, err string, code int, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
 }
