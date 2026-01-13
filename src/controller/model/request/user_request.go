f725163ce442edb4bd346dc1814b73beca61be31
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2026-01-13 19:56:48 -0300

    Refactor user controller: remove unused functions and update UserRequest model for optional fields

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Name     string `json:"name" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
-	Age      int8   `json:"age" binding:"required,gte=0,lte=130"`
-	Password string `json:"password" binding:"required,min=6"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Age      int8   `json:"age"`
+	Password string `json:"password"`
 }
