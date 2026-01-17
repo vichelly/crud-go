346da2ad817e781af277ebcf5b83a9a6342138ef
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2026-01-16 22:39:17 -0300

    Add validation tags to UserRequest and UserResponse models for improved data integrity

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Age      int8   `json:"age"`
-	Password string `json:"password"`
+	Name     string `json:"name" binding:"required,min=3,max=50"`
+	Email    string `json:"email" binding:"required,email"`
+	Age      int8   `json:"age" binding:"required"`
+	Password string `json:"password" binding:"required,containsany=!@#$%^&*(),min=4,max=20"`
 }
--- src/controller/model/response/user_response.go
+++ src/controller/model/response/user_response.go
@@ -1,8 +1,8 @@
 package response
 
 type UserResponse struct {
-	ID    uint   `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	Age   int8   `json:"age"`
+	ID    uint   `json:"id" binding:"required"`
+	Name  string `json:"name" binding:"required"`
+	Email string `json:"email" binding:"required"`
+	Age   int8   `json:"age" binding:"required"`
 }
